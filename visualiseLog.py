9c782de303c292b546ff85bcacc907bd5a0c5e18
Bumblebee Mountain <BumblebeeMountain@gmail.com>
2022-05-27 12:18:12 +0100

    Change day visualisations to now run from 6am - 6am

--- visualiseLog.py
+++ visualiseLog.py
@@ -23,17 +23,22 @@ def plotLog(log):
 
     plotGraph(timeArr, dayIndexes, dayLabels, grid=True)
 
+def rotateArr(arr):
+    arr.append(arr.pop(0))
+
 def plotDay(log, dayIndex):
     timeArr = log.getLog()
     res = log.getRes()
 
-    dayIndexes = [log.timeToIndex("00:00", i) for i in [dayIndex, dayIndex+1]]
+    dayIndexes = [log.timeToIndex("06:00", i) for i in [dayIndex, dayIndex+1]]
     dayLabels = np.array(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
 
     arrToPlot = timeArr[dayIndexes[0]: dayIndexes[1]]
 
     xTicks = [i for i in range(0, len(arrToPlot)+1, int(60/log.getRes()*2))]
     xTickLabels = [f"0{c}:00" for c in range(0, 9, 2)] + [f"{c}:00" for c in range(10, 25, 2)]
+    for i in range(3):
+        rotateArr(xTickLabels)
     xTickLabels[0] = dayLabels[dayIndex]
     xTickLabels[-1] = dayLabels[(dayIndex+1)%8]
 
@@ -59,13 +64,18 @@ def plotGraph(arrToPlot, xTicks=None, xTickLabels=None, xLabel="Time", yLabel="O
 def avgDay(log):
     timeArr = np.array(log.getLog()).reshape((7,-1))
     avg = np.mean(timeArr, axis=0)
-    plotGraph(avg, grid=False)
+
+    xTicks = [i for i in range(0, len(avg)+1, int(60/log.getRes()*2))]
+    xTickLabels = [f"0{c}:00" for c in range(0, 9, 2)] + [f"{c}:00" for c in range(10, 25, 2)]
+    plotGraph(avg, xTicks, xTickLabels, yLabel="Average Occupancy")
 
 def main():
     diaLogName = "diaLog.db"
+    dayDict = {"mon":0, "tue":1, "wed":2, "thur":3, "fri":4, "sat":5, "sun":6}
+
     log = getLog(diaLogName)
-    plotLog(log)
-    # plotDay(log, 4)
+    # plotLog(log)
+    plotDay(log, dayDict["fri"])
     # avgDay(log)
 
 if __name__ == "__main__":
